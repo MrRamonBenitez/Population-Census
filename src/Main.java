a5dc80787c68c95b92fda5b5b30f37f8a6adebaa
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-01-02 20:54:55 +0500

    Main.java modyfied

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +15,37 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+
+        long count = persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count();
+
+        System.out.println(count);
+
+        List<String> conscripts = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN)
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() <= 27)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+
+        System.out.println(conscripts);
+
+        List<Person> potentiallyWorkablePeople = persons.stream()
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> (person.getSex() == Sex.MAN && person.getAge() <= 65)
+                                    || (person.getSex() == Sex.WOMAN && person.getAge() <= 60))
+                .sorted(Comparator.comparing(Person::getFamily, String.CASE_INSENSITIVE_ORDER))
+                .collect(Collectors.toList());
+
+        System.out.println(potentiallyWorkablePeople);
+
+
+
+
+
+
 
 
     }
